80b5424f2e8efde5da193049f189a649d55b8fea
Fvello <fabricio.vello@ftc.edu.br>
2022-10-07 09:14:08 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+require('dotenv').config();
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  
+  const page = await browser.newPage();
+  await page.goto(`https://ondeestameupedido.com.br/${process.env.trackcode}`);
+  await page.waitForSelector('.tracking-content');
+  //await page.screenshot({path: 'track.png'});
+  let status = await page.evaluate(() =>{
+    let step = document.querySelectorAll('.tracking-item');
+    let stepArray = Array.from(step);
+    return stepArray.map(now => now.textContent)
+  });
+  console.log(status);
+  await browser.close();
+})();
